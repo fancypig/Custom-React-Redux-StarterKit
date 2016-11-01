a620c9fc271954a75319850807963ce71e215055
Percy Teng <percytsy@gmail.com>
2016-10-31 21:53:38 -0400

    understanding structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React , { Component } from 'react'
+import React from 'react'
 import { render } from 'react-dom'
 import {Provider} from 'react-redux'
 import { createStore } from 'redux'
 import allReducers from './reducers/index'
 import App from './containers'
-import styles from './style.css'
-
 const store = createStore(allReducers)
 
 render(
     <Provider store={store}>
         <App/>
     </Provider>
-
     ,document.getElementById("app")
 )
